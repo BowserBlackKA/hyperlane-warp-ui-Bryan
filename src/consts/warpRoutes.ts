18c6e2a58a231af88fca8124630ae1dfe51214a8
Bryan Kaufmann <bryankaufmann@mbp-de-bryan.home>
2024-10-11 21:10:24 +0200

    feat: customize styles and components

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -7,3 +7,31 @@ export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [],
   options: {},
 };
+
+export const warpRoutes = [
+  {
+    cchainName: "arbitrum",
+    standard: "EvmHypCollateral",
+    decimals: 18,
+    symbol: "WETH",
+    name: "Wrapped Ether",
+    logoURI: "/Users/bryankaufmann/hyperlane-warp-ui-Bryan/public/logos/weth.png",
+    addressOrDenom: "0x2dBFfbc5447A372DfD278704541652DA010498c1",
+    collateralAddress: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+    connections: [
+      { token: "ethereum|base|0x2dBFfbc5447A372DfD278704541652DA010498c1" }
+    ]
+  },
+  {
+    chainName: "base",
+    standard: "EvmHypSynthetic",
+    decimals: 18,
+    symbol: "WETH",
+    name: "Wrapped Ether",
+    logoURI: "/Users/bryankaufmann/hyperlane-warp-ui-Bryan/public/logos/weth.png",
+    addressOrDenom: "0x2dBFfbc5447A372DfD278704541652DA010498c1",
+    connections: [
+      { token: "ethereum|arbitrum|0x2dBFfbc5447A372DfD278704541652DA010498c1" }
+    ]
+  }
+];
